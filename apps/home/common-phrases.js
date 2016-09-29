94bedd37276d8110ae4647ff20b853c8a55498e2
Ashley Russell <arussell@clearvision-cm.com>
2016-09-29 17:06:29 +0100

    WIP

---
+++ apps/home/common-phrases.js
@@ -0,0 +1,6 @@
+module.exports = {
+    start: "play|put on|watch|start playing|start",
+    stop: "stop|turn off|stop playing",
+    films: "rocky|the wolf of wallstreet|fight club",
+    musicGenres: "house|dance|drum and bass|piano|chill|chill out|reggae"
+};
