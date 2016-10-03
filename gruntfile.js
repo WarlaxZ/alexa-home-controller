11bda50bf43ad5645801d5db2edc607cddda9e87
Ashley Russell <arussell@clearvision-cm.com>
2016-10-03 12:21:27 +0100

    Might help if I put things in the right place . . . :P

---
+++ gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        // This line makes your node configurations available for use
+        pkg: grunt.file.readJSON('package.json'),
+        // This is where we configure JSHint
+        jshint: {
+            // You get to make the name
+            // The paths tell JSHint which files to validate
+            all: ['./**/*.js'],
+             options: {
+                 esversion: 6,
+                 node: true,
+//                 jshintrc: true,
+                 ignores: [
+                    'node_modules/**/*.js',
+                    'apps/**/node_modules/**/*.js'
+                ]
+             }
+         }
+     });
+    // Each plugin must be loaded following this pattern
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+
+};
